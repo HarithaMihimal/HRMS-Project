d5a71ccb79eba6be1868b455ffbb5c720ccb2f87
deshitha-g <deshithagallage623@gmail.com>
2023-09-24 17:08:52 +0530

    Added ER diagram and Updated Table Script.sql

--- Database/Table Script.sql
+++ Database/Table Script.sql
@@ -2,8 +2,8 @@ create database HRMS;
 use hrms;
 
 CREATE TABLE `Supervisor` (
-  `Supervisor_ID` VARCHAR(255) NOT NULL,
-  `Subordinate_ID` VARCHAR(255) NOT NULL,
+  `Supervisor_ID` INT(12) NOT NULL,
+  `Subordinate_ID` INT(12) NOT NULL,
   PRIMARY KEY (`Supervisor_ID`, `Subordinate_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -15,17 +15,6 @@ CREATE TABLE `Dependent_Information` (
   `Relation` VARCHAR(255) NOT NULL,
   `Gender` ENUM('Male', 'Female', 'Prefer not to say') NOT NULL,
   PRIMARY KEY (`Dependent_ID`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `Leave_Request` (
-  `Leave_Req_ID` INT(12) NOT NULL AUTO_INCREMENT,
-  `Employee_ID` INT(12) NOT NULL,
-  `Start_Date` DATE NOT NULL,
-  `No_of_Days` INT(3) NOT NULL,
-  `Type` ENUM('Annual', 'Casual', 'Maternity', 'No-pay') NOT NULL,
-  `Status` ENUM('Approved', 'Pending', 'Rejected') NOT NULL,
-  PRIMARY KEY (`Leave_Req_ID`),
-  FOREIGN KEY (`Employee_ID`) REFERENCES `Employee_Data`(`Employee_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `Branch` (
@@ -46,8 +35,14 @@ CREATE TABLE `Department` (
 CREATE TABLE `Pay_Grade` (
   `Pay_Grade_ID` INT(3) NOT NULL AUTO_INCREMENT,
   `Pay_Grade` VARCHAR(255) NOT NULL,
-  `Basic _salary` NUMERIC(10 ,2) NOT NULL,
+  `Basic_salary` NUMERIC(10 ,2) NOT NULL,
   PRIMARY KEY (`Pay_Grade_ID`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `Employment_Status` (
+  `Status_ID` INT(3) NOT NULL AUTO_INCREMENT,
+  `Status` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`Status_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `Employee_Data` (
@@ -57,7 +52,7 @@ CREATE TABLE `Employee_Data` (
   `Marital_status` ENUM('Married', 'Unmarried', 'Other', 'Prefer not to say') NOT NULL,
   `Birthday` DATE NOT NULL,
   `Email` VARCHAR(255),
-  `Employment_status`  INT(12) NOT NULL,
+  `Employment_status`  INT(3) NOT NULL,
   `Job_Title` ENUM('HR Manager', 'Accountant', 'Software Engineer', 'QA Engineer') NOT NULL,
   `Pay_Grade_ID` INT(3) NOT NULL,
   `Branch_No` INT(12) NOT NULL,
@@ -71,6 +66,17 @@ CREATE TABLE `Employee_Data` (
   FOREIGN KEY (`Employment_status`) REFERENCES `Employment_Status`(`Status_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `Leave_Request` (
+  `Leave_Req_ID` INT(12) NOT NULL AUTO_INCREMENT,
+  `Employee_ID` INT(12) NOT NULL,
+  `Start_Date` DATE NOT NULL,
+  `No_of_Days` INT(3) NOT NULL,
+  `Type` ENUM('Annual', 'Casual', 'Maternity', 'No-pay') NOT NULL,
+  `Status` ENUM('Approved', 'Pending', 'Rejected') NOT NULL,
+  PRIMARY KEY (`Leave_Req_ID`),
+  FOREIGN KEY (`Employee_ID`) REFERENCES `Employee_Data`(`Employee_ID`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `Leave_Limit` (
   `Pay_Grade_ID` INT(3) NOT NULL,
   `Annual` INT(3) NOT NULL,
@@ -81,6 +87,12 @@ CREATE TABLE `Leave_Limit` (
   FOREIGN KEY (`Pay_Grade_ID`) REFERENCES `Pay_Grade`(`Pay_Grade_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE `Custom_Attribute_Definition` (
+  `Attribute_ID` INT(3) NOT NULL AUTO_INCREMENT,
+  `Attribute_Name` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`Attribute_ID`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE `Employee_Custom_Attribute` (
   `Attribute_ID` INT(3) NOT NULL,
   `Employee_ID` INT(12) NOT NULL,
@@ -88,12 +100,6 @@ CREATE TABLE `Employee_Custom_Attribute` (
   PRIMARY KEY (`Attribute_ID`, `Employee_ID`),
   FOREIGN KEY (`Employee_ID`) REFERENCES `Employee_Data`(`Employee_ID`),
   FOREIGN KEY (`Attribute_ID`) REFERENCES `Custom_Attribute_Definition`(`Attribute_ID`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE `Custom_Attribute_Definition` (
-  `Attribute_ID` INT(3) NOT NULL AUTO_INCREMENT,
-  `Attribute_Name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`Attribute_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE `Contact_Number_Details` (
@@ -122,11 +128,12 @@ CREATE TABLE `Employee_Account` (
   FOREIGN KEY (`Employee_ID`) REFERENCES `Employee_Data`(`Employee_ID`)
 )ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE `Employment_Status` (
-  `Status_ID` INT(12) NOT NULL AUTO_INCREMENT,
-  `Status` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`Status_ID`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
+CREATE TABLE `Organization` (
+  `Registration_No` VARCHAR(255) NOT NULL,
+  `Name` VARCHAR(255) NOT NULL,
+  `Main_Branch` INT(5) NOT NULL,
+  PRIMARY KEY (`Registration_No`),
+  FOREIGN KEY (`Main_Branch`) REFERENCES `Branch`(`Branch_No`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 show tables;
