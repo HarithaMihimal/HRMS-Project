fe112a485f850ac4e3c10cadbb98a6778513a2fa
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2023-09-26 12:26:22 +0530

    Update Triggers.sql
    Updated the trigger function so that it works for all leave types

--- Database/Triggers.sql
+++ Database/Triggers.sql
@@ -11,10 +11,55 @@ BEGIN
         WHERE Employee_ID = NEW.Employee_ID;
 
         IF @annual_leave_count IS NOT NULL THEN
-            SET @annual_leave_count := @annual_leave_count + 10;
+            SET @annual_leave_count := @annual_leave_count + NEW.No_Of_Days;
 
             UPDATE employee_leave_count
             SET Annual = @annual_leave_count
+            WHERE Employee_ID = NEW.Employee_ID;
+        END IF;
+
+    ELSE IF  NEW.Status = 'Approved' AND NEW.Type = 'Casual' THEN
+        SET @casual_leave_count := 0;
+
+        SELECT Casual INTO @casual_leave_count
+        FROM employee_leave_count
+        WHERE Employee_ID = NEW.Employee_ID;
+
+        IF @casual_leave_count IS NOT NULL THEN
+            SET @casual_leave_count := @casual_leave_count + NEW.No_Of_Days;
+
+            UPDATE employee_leave_count
+            SET Casual = @casual_leave_count
+            WHERE Employee_ID = NEW.Employee_ID;
+        END IF;
+
+    ELSE IF  NEW.Status = 'Approved' AND NEW.Type = 'No Pay' THEN
+        SET @no_pay_leave_count := 0;
+
+        SELECT No_Pay INTO @no_pay_leave_count
+        FROM employee_leave_count
+        WHERE Employee_ID = NEW.Employee_ID;
+
+        IF @no_pay_leave_count IS NOT NULL THEN
+            SET @no_pay_leave_count := @no_pay_leave_count + NEW.No_Of_Days;
+
+            UPDATE employee_leave_count
+            SET No_Pay = @no_pay_leave_count
+            WHERE Employee_ID = NEW.Employee_ID;
+        END IF;
+
+    ELSE IF  NEW.Status = 'Approved' AND NEW.Type = 'Maternity Leave' THEN
+        SET @maternity_leave_count := 0;
+
+        SELECT Maternity_Leave INTO @maternity_leave_count
+        FROM employee_leave_count
+        WHERE Employee_ID = NEW.Employee_ID;
+
+        IF @maternity_leave_count IS NOT NULL THEN
+            SET @maternity_leave_count := @maternity_leave_count + NEW.No_Of_Days;
+
+            UPDATE employee_leave_count
+            SET Maternity_Leave = @maternity_leave_count
             WHERE Employee_ID = NEW.Employee_ID;
         END IF;
     END IF;
