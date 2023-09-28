e860829eb6c7fdde0bdff6bd1799fea542cade32
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2023-09-29 00:00:29 +0530

    Create Dependent_Information.sql

---
+++ Database/SampleData/Dependent_Information.sql
@@ -0,0 +1,41 @@
+
+
+-- Insert sample data into the Dependent_Information table
+INSERT INTO `Dependent_Information` (`First_name`, `Last_name`, `Age`, `Relation`, `Gender`)
+VALUES
+('John', 'Doe', 10, 'Child', 'Male'),
+('Jane', 'Smith', 35, 'Spouse', 'Female'),
+('David', 'Johnson', 62, 'Parent', 'Male'),
+('Emily', 'Brown', 28, 'Spouse', 'Female'),
+('Michael', 'Williams', 5, 'Child', 'Male'),
+('Sophia', 'Miller', 42, 'Spouse', 'Female'),
+('Ethan', 'Anderson', 8, 'Child', 'Male'),
+('Olivia', 'Garcia', 19, 'Child', 'Female'),
+('Aiden', 'Harris', 14, 'Child', 'Male'),
+('Liam', 'Martinez', 28, 'Spouse', 'Male'),
+('Abigail', 'Jones', 6, 'Child', 'Female'),
+('Noah', 'Davis', 32, 'Spouse', 'Male'),
+('Isabella', 'Wilson', 60, 'Parent', 'Female'),
+('James', 'Taylor', 11, 'Child', 'Male'),
+('Charlotte', 'Brown', 2, 'Child', 'Female'),
+('Benjamin', 'Clark', 7, 'Child', 'Male'),
+('Amelia', 'Lewis', 50, 'Parent', 'Female'),
+('Lucas', 'Perez', 12, 'Child', 'Male'),
+('Harper', 'White', 4, 'Child', 'Female'),
+('Mason', 'Johnson', 9, 'Child', 'Male'),
+('Evelyn', 'Thomas', 55, 'Parent', 'Female'),
+('Oliver', 'Harris', 15, 'Child', 'Male'),
+('Ava', 'Moore', 3, 'Child', 'Female'),
+('William', 'Davis', 37, 'Spouse', 'Male'),
+('Sophia', 'Hall', 1, 'Child', 'Female'),
+('Henry', 'Smith', 30, 'Spouse', 'Male'),
+('Mia', 'Anderson', 5, 'Child', 'Female'),
+('Daniel', 'Jackson', 65, 'Parent', 'Male'),
+('Lily', 'Wilson', 13, 'Child', 'Female'),
+('Michael', 'Clark', 20, 'Child', 'Male'),
+('Emily', 'Garcia', 29, 'Spouse', 'Female'),
+('Matthew', 'Lee', 16, 'Child', 'Male'),
+('Emma', 'Allen', 7, 'Child', 'Female'),
+('Alexander', 'Moore', 45, 'Parent', 'Male');
+
+-- select * from Dependent_Information;
