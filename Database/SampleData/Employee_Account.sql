c4f0f9d370a85e2f70829565ebc1fdae148d77a0
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2023-09-29 00:08:16 +0530

    Create Employee_Account.sql

---
+++ Database/SampleData/Employee_Account.sql
@@ -0,0 +1,36 @@
+
+-- Insert sample data into the Employee_Account table
+INSERT INTO `Employee_Account` (`User_ID`, `Employee_ID`, `Password`) VALUES
+('user123', 1, 'passw0rd!'),
+('employee456', 2, 'SecurePwd789'),
+('johndoe77', 3, 'R@nd0mPass'),
+('user987', 4, 'P@ssw0rd123'),
+('emplacer', 5, 'SecretPwd456'),
+('accountant22', 6, 'MyP@ss123'),
+('janesmith33', 7, 'RandomPwd!'),
+('user789', 8, 'StrongP@ss456'),
+('engineer42', 9, '1234Random!'),
+('user555', 10, 'Pwd789!'),
+('manager66', 11, 'Random123!'),
+('employee101', 12, 'Password!123'),
+('user202', 13, 'Secure123!'),
+('emplacer303', 14, 'StrongPwd!789'),
+('user404', 15, '123Random!'),
+('analyst505', 16, 'RandomPwd!456'),
+('user606', 17, 'P@ssw0rd789'),
+('assistant707', 18, 'SecurePwd!123'),
+('user808', 19, 'RandomP@ss123'),
+('coordinator909', 20, 'Pwd123!'),
+('user1010', 21, 'Random1234!'),
+('designer1111', 22, '12345Pwd!'),
+('user1212', 23, 'P@ssword!789'),
+('developer1313', 24, 'StrongPwd123!'),
+('user1414', 25, 'RandomP@sswd!'),
+('administrator1515', 26, 'Pwd!Random456'),
+('user1616', 27, 'P@sswd!1234'),
+('supervisor1717', 28, 'SecurePwd!567'),
+('user1818', 29, 'RandomP@ssword!'),
+('manager1919', 30, '12345678Pwd!');
+
+
+-- select * from Employee_Account;
