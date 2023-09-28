f48b50723a22dda0efe984b7bfbb021ea55e9a60
Haritha Mihimal Wilwala Arachchi <129370991+HarithaMihimal@users.noreply.github.com>
2023-09-29 00:08:45 +0530

    Create Organization.sql

---
+++ Database/SampleData/Organization.sql
@@ -0,0 +1,36 @@
+
+-- Insert more sample data into the Organization table with registration numbers
+
+INSERT INTO `Organization` (`Registration_No`, `Name`, `Main_Branch`) VALUES
+('ORG/2023/001', 'XYZ Corporation', 101),
+('ORG/2023/002', 'ABC Ltd.', 102),
+('ORG/2023/003', 'XYZ Corp.', 103),
+('ORG/2023/004', 'LMN Inc.', 104),
+('ORG/2023/005', 'PQR Enterprises', 105),
+('ORG/2023/006', 'JKL Co.', 106),
+('ORG/2023/007', 'EFG Group', 107),
+('ORG/2023/008', 'RST Ltd.', 108),
+('ORG/2023/009', 'UVW Corporation', 109),
+('ORG/2023/010', 'HIJ Inc.', 110),
+('ORG/2023/011', 'NOP Enterprises', 111),
+('ORG/2023/012', 'GHI Co.', 112),
+('ORG/2023/013', 'XYZ Corporation', 113),
+('ORG/2023/014', 'ABC Ltd.', 114),
+('ORG/2023/015', 'XYZ Corp.', 115),
+('ORG/2023/016', 'LMN Inc.', 116),
+('ORG/2023/017', 'PQR Enterprises', 117),
+('ORG/2023/018', 'JKL Co.', 118),
+('ORG/2023/019', 'EFG Group', 119),
+('ORG/2023/020', 'RST Ltd.', 120),
+('ORG/2023/021', 'UVW Corporation', 121),
+('ORG/2023/022', 'HIJ Inc.', 122),
+('ORG/2023/023', 'NOP Enterprises', 123),
+('ORG/2023/024', 'GHI Co.', 124),
+('ORG/2023/025', 'XYZ Corporation', 125),
+('ORG/2023/026', 'ABC Ltd.', 126),
+('ORG/2023/027', 'XYZ Corp.', 127),
+('ORG/2023/028', 'LMN Inc.', 128),
+('ORG/2023/029', 'PQR Enterprises', 129),
+('ORG/2023/030', 'JKL Co.', 130);
+
+-- select * from Organization;
