d099a04634aef120695be7b3a342e8f1548de672
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2023-10-01 01:32:55 +0530

    Created the frontend to request a leave

--- client/src/LeaveReq.jsx
+++ client/src/LeaveReq.jsx
@@ -1,9 +1,75 @@
 import React from "react";
+import { useState } from "react";
+import "./style.css";
 
 function LeaveReq() {
+  const [id, setid] = useState("");
+  const [startDate, setStartDate] = useState(new Date());
+  const [day_no, setNumDays] = useState(0);
+  const [type, setType] = useState("");
+
   return (
-    <div>
-      <h1>Here you can request a leave</h1>
+    <div className="container mt-5">
+      <div className="information">
+        <div className="mb-3">
+          <label htmlFor="employeeId" className="form-label">
+            Employee ID:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="employeeId"
+            onChange={(event) => {
+              setid(event.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="startDate" className="form-label">
+            Start Date:
+          </label>
+          <input
+            type="date"
+            className="form-control"
+            id="startDate"
+            onChange={(event) => {
+              setStartDate(event.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="numDays" className="form-label">
+            Number of Days:
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="numDays"
+            onChange={(event) => {
+              setNumDays(event.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="leaveType" className="form-label">
+            Leave Type:
+          </label>
+          <select
+            className="form-control"
+            id="leaveType"
+            onChange={(event) => {
+              setType(event.target.value);
+            }}
+          >
+            <option value="">Select Leave Type</option>
+            <option value="annual">Annual</option>
+            <option value="casual">Casual</option>
+            <option value="no_pay">No Pay</option>
+            <option value="maternity">Maternity</option>
+          </select>
+        </div>
+        <button className="btn btn-primary">Submit Request</button>
+      </div>
     </div>
   );
 }
