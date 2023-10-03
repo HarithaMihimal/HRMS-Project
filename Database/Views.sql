7ebe413956fae5820e0dd4281ed0ef15a0fb7bad
Vinu Kaveesha <vinukaveesha2001@gmail.com>
2023-10-03 16:50:58 +0530

    create view for required emp details

---
+++ Database/Views.sql
@@ -0,0 +1,14 @@
+create view emp_view AS
+select
+	t1.Employee_ID,
+    t1.First_name,
+    t1.Last_name,
+    t1.Job_Title,
+    t2.dept_name,
+    t3.pay_grade
+from
+    employee_data AS t1
+left join
+    department AS t2 ON t1.Dept_ID = t2.Dept_ID
+left join
+	pay_grade AS t3 ON t1.Pay_Grade_ID = t3.Pay_Grade_ID;
