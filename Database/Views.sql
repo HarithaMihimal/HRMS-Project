9ee03994b00d2e78ca63061232f23ad5111610ba
Isara Liyanage <136115146+Isara-Li@users.noreply.github.com>
2023-10-04 18:44:50 +0530

    Update Views.sql
    Created a view to check the username and password in the login page

--- Database/Views.sql
+++ Database/Views.sql
@@ -12,3 +12,9 @@ left join
     department AS t2 ON t1.Dept_ID = t2.Dept_ID
 left join
 	pay_grade AS t3 ON t1.Pay_Grade_ID = t3.Pay_Grade_ID;
+
+
+create view password_check as
+select Employee_ID,User_ID,Password,Job_Title 
+from employee_account join employee_data 
+using (Employee_ID);
