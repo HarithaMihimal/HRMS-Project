6d4aa344691413c957f4f48ad635f92f0a755d59
deshitha-g <deshithagallage623@gmail.com>
2023-10-17 15:05:35 +0530

    Corrected row height error

--- WebApp/client/src/EmployeeManagement.jsx
+++ WebApp/client/src/EmployeeManagement.jsx
@@ -1,39 +1,51 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
-import { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import ReactPaginate from 'react-paginate';
+import './EmployeeManagement.css'; // Import the CSS file
 
 function EmployeeManagement() {
   const { id_to_transfer } = useParams();
-  console.log('id_to_transfer in EmployeeManagement:', id_to_transfer);
-  const navigate = useNavigate(); // Define the navigate function
+  const navigate = useNavigate();
 
-  // Handler for the "AddEmployee" button click
   const handleAddEmployee = () => {
     navigate(`/PageHR/${id_to_transfer}/EmployeeManagement/AddEmployee`);
   };
 
-  const [employees, setEmployees] = useState([]); // Define the employees state variable and the setEmployees function
+  const [employees, setEmployees] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0); // Current page number
+  const perPage = 10; // Number of records per page
+
   useEffect(() => {
     Axios.get('http://localhost:3000/employeeData')
-    .then((response) => setEmployees(response.data))
-    .catch((error) => console.log(error));
+      .then((response) => setEmployees(response.data))
+      .catch((error) => console.log(error));
   }, []);
 
-  // Custom CSS for reducing row height
-  const tableCellStyle = {
-    verticalAlign: 'middle', // Center content vertically
-    padding: '0px', // Adjust padding as needed
+  const offset = currentPage * perPage;
+  const pageCount = Math.ceil(employees.length / perPage);
+
+  const handlePageClick = ({ selected }) => {
+    setCurrentPage(selected);
   };
 
+  const customButtonLabel = (label) => {
+    return (
+      <button className="custom-pagination-button">
+        {label}
+      </button>
+    );
+  };
+
+  const displayedEmployees = employees.slice(offset, offset + perPage);
+
   return (
-    <div className='container'>
+    <div className="container">
       <h1>Page the Employee Management</h1>
       <div className="justify-content-center">
         <div>
           <button type="button" className="btn btn-success my-3" onClick={handleAddEmployee}>
-            Add Employee
+            Add New Employee
           </button>
         </div>
 
@@ -50,24 +62,25 @@ function EmployeeManagement() {
               </tr>
             </thead>
             <tbody>
-              {employees.map((employee) => (
+              {displayedEmployees.map((employee) => (
                 <tr key={employee.Employee_ID}>
-                  <td style={tableCellStyle}>{employee.Employee_ID}</td>
+                  <td>{employee.Employee_ID}</td>
                   <td>{employee.First_Name + " " + employee.Last_Name}</td>
                   <td>{employee.Gender}</td>
                   <td>{employee.Job_Title}</td>
                   <td>{employee.Email}</td>
                   <td>
-                    <div className="d-flex">
-                      <dic>
-                        <button type="button" className="btn btn-success">View</button>
-                      </dic>
-                      <dic className="mx-1">
-                        <button type="button" className="btn btn-primary">Edit</button>
-                      </dic>
-                      <dic>
-                        <button type="button" className="btn btn-danger">Delete</button>
-                      </dic>
+                    <div style={{margin: "0px"}} className="d-flex custom-button-container">
+                      <div style={{margin: "0px"}} className="mx-1">
+                        <button type="button" className="btn btn-success">
+                          View
+                        </button>
+                      </div>
+                      <div style={{margin: "0px"}} className="mx-1">
+                        <button type="button" className="btn btn-warning">
+                          Edit
+                        </button>
+                      </div>
                     </div>
                   </td>
                 </tr>
@@ -75,6 +88,21 @@ function EmployeeManagement() {
             </tbody>
           </table>
         </div>
+      </div>
+
+      <div className="pagination-container">
+        <ReactPaginate
+          previousLabel={customButtonLabel('Previous')}
+          nextLabel={customButtonLabel('Next')}
+          breakLabel={<span>...</span>}
+          pageCount={pageCount}
+          marginPagesDisplayed={2}
+          pageRangeDisplayed={5}
+          onPageChange={handlePageClick}
+          containerClassName="pagination"
+          subContainerClassName="pages pagination"
+          activeClassName="active"
+        />
       </div>
     </div>
   );
