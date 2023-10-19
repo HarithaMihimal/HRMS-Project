95512f4f5efbede092d992eb1c192ec363491926
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2023-10-19 10:55:00 +0530

    UI changes

--- WebApp/client/src/AddCustom.jsx
+++ WebApp/client/src/AddCustom.jsx
@@ -12,19 +12,14 @@ function AddCustom() {
   const [successMessage, setSuccessMessage] = useState('');
   const [successMessage2, setSuccessMessage2] = useState('');
 
-
   useEffect(() => {
     const storedSuccessMessage = localStorage.getItem('successMessage');
 
     if (storedSuccessMessage) {
-      // Display the success message.
       setSuccessMessage(storedSuccessMessage);
-
-      // Clear the success message from local storage.
       localStorage.removeItem('successMessage');
     }
-    // Fetch the list of custom attribute definitions from your API or database
-    // and set it in the customAttributes state.
+
     axios.get('http://localhost:3000/customAttributes')
       .then((response) => {
         setCustomAttributes(response.data);
@@ -34,28 +29,21 @@ function AddCustom() {
 
   const handleAddCustomAttributeDefinition = () => {
     if (customAttributeName) {
-      // Send a POST request to your API to add the custom attribute definition.
       axios.post('http://localhost:3000/createCustomAttribute', {
         attributeName: customAttributeName,
       })
         .then((response) => {
-          // Handle success or show a notification to the user.
           console.log('Custom attribute definition added:', response.data);
-          // You can also update the UI to reflect the added definition.
-          // After adding the definition, it will appear in the dropdown for selection.
           setCustomAttributes([...customAttributes, response.data]);
           setSelectedAttributeID(response.data.Attribute_ID);
           localStorage.setItem('successMessage', 'The Attribute added successfully');
-          window.location.reload();
           setSuccessMessage('The attribute was added successfully.');
-         
         })
         .catch((error) => {
           console.error('Error adding custom attribute definition:', error);
-          setSuccessMessage('Error adding custom attribute to employee.');
+          setSuccessMessage('Error adding custom attribute to the employee.');
         });
     } else {
-      // Handle validation or show an error message.
       console.error('Invalid input');
       setSuccessMessage('Invalid input. Please check your input fields.');
     }
@@ -63,88 +51,90 @@ function AddCustom() {
 
   const handleAddCustomAttributeToEmployee = () => {
     if (employeeID && selectedAttributeID && customAttributeValue) {
-      // Send a POST request to your API to associate the custom attribute with the employee
-      // and set its value.
       axios.post('http://localhost:3000/associateCustomAttribute', {
         employeeID: employeeID,
         attributeID: selectedAttributeID,
         value: customAttributeValue,
       })
         .then((response) => {
-          // Handle success or show a notification to the user.
-          console.log('Custom attribute added to employee:', response.data);
-          // You can also update the UI to reflect the added custom attribute for the employee.
+          console.log('Custom attribute added to the employee:', response.data);
           setSuccessMessage2('Custom attribute value added to the employee successfully.');
         })
-        .catch((error) => console.error('Error adding custom attribute to employee:', error));
+        .catch((error) => console.error('Error adding custom attribute to the employee:', error));
     } else {
-      // Handle validation or show an error message.
       console.error('Invalid input');
     }
   };
 
   return (
-    <div>
-      <h1>Add Custom Attribute</h1>
-      <label>
-        Custom Attribute Name:
-        <input
-          type="text"
-          value={customAttributeName}
-          onChange={(e) => setCustomAttributeName(e.target.value)}
-        />
-      </label>
-      <br />
-      <button onClick={handleAddCustomAttributeDefinition}>Add Custom Attribute Definition</button>
-      <br />
-      {successMessage && (
-      <div className="success-message">
-        {successMessage}
-      </div>
-    )}
-      <label>
-        EmployeeID:
-        <input
-          type="text"
-          value={employeeID}
-          onChange={(e) => setEmployeeID(e.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Select Attribute:
-        <select
-          value={selectedAttributeID}
-          onChange={(e) => setSelectedAttributeID(e.target.value)}
-        >
-          <option value="">Select Attribute</option>
-          {customAttributes.map((attribute) => (
-            <option key={attribute.Attribute_ID} value={attribute.Attribute_ID}>
-              {attribute.Attribute_Name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <br />
-      <label>
-        Custom Attribute Value:
-        <input
-          type="text"
-          value={customAttributeValue}
-          onChange={(e) => setCustomAttributeValue(e.target.value)}
-        />
-      </label>
-      <br />
-      <button onClick={handleAddCustomAttributeToEmployee}>Add Custom Attribute to Employee</button>
-      {successMessage && (
-        <div className="success-message">
-          {successMessage2}
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-4">
+          <h3 className="mb-4 text-center">Add Custom Attribute</h3>
+          {successMessage && (
+            <div className="alert alert-success">{successMessage}</div>
+          )}
+          <div className="container">
+            <form>
+              <div className="mb-3">
+                <label htmlFor="customAttributeName" className="form-label">Custom Attribute Name:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="customAttributeName"
+                  value={customAttributeName}
+                  onChange={(e) => setCustomAttributeName(e.target.value)}
+                />
+              </div>
+              <button className="btn btn-primary" onClick={handleAddCustomAttributeDefinition}>Add Custom Attribute Definition</button>
+            </form>
+            <form>
+              <div className="mt-3">
+                <label htmlFor="employeeID" className="form-label">Employee ID:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="employeeID"
+                  value={employeeID}
+                  onChange={(e) => setEmployeeID(e.target.value)}
+                />
+              </div>
+              <div className="mt-3">
+                <label htmlFor="selectAttribute" className="form-label">Select Attribute:</label>
+                <select
+                  className="form-select"
+                  id="selectAttribute"
+                  value={selectedAttributeID}
+                  onChange={(e) => setSelectedAttributeID(e.target.value)}
+                >
+                  <option value="">Select Attribute</option>
+                  {customAttributes.map((attribute) => (
+                    <option key={attribute.Attribute_ID} value={attribute.Attribute_ID}>
+                      {attribute.Attribute_Name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className="mt-3">
+                <label htmlFor="customAttributeValue" className="form-label">Custom Attribute Value:</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id="customAttributeValue"
+                  value={customAttributeValue}
+                  onChange={(e) => setCustomAttributeValue(e.target.value)}
+                />
+              </div>
+              <button className="btn btn-primary mt-3" onClick={handleAddCustomAttributeToEmployee}>Add Custom Attribute to Employee</button>
+            </form>
+            {successMessage2 && (
+              <div className="alert alert-success mt-3">{successMessage2}</div>
+            )}
+          </div>
         </div>
-      )}
+      </div>
     </div>
   );
 }
-
-
 
 export default AddCustom;
---
+++ WebApp/client/src/Components/ReportCard2.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { useNavigate, useParams } from 'react-router-dom';
+
+function ReportCard2() {
+  const { id_to_transfer } = useParams();
+  const navigate = useNavigate();
+
+  const handleGenerateReport = () => {
+    navigate(`/PageHR/${id_to_transfer}/ReportGeneration/Report1`);
+  };
+
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>Report 1</Card.Title>
+        <Card.Text>Generate Report 1</Card.Text>
+        <Button variant="primary" onClick={handleGenerateReport}>Generate Report</Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ReportCard2;
