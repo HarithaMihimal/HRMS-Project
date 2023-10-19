a7ea119b46045eb7948aa527751d6eeb942b551d
Isara-Li <vimandika.21@cse.mrt.ac.lk>
2023-10-19 11:06:47 +0530

    Update ReportCard1.jsx

--- WebApp/client/src/Components/ReportCard1.jsx
+++ WebApp/client/src/Components/ReportCard1.jsx
@@ -15,7 +15,7 @@ function ReportCard1() {
     <Card style={{ width: '18rem' }}>
       <Card.Body>
         <Card.Title>Leave Report</Card.Title>
-        <Card.Text>Gives an overw</Card.Text>
+        <Card.Text>  Generate a report of all the leaves taken by an employee grouped by the respective department. </Card.Text>
         <Button variant="primary" onClick={handleGenerateReport}>Generate Report</Button>
       </Card.Body>
     </Card>
